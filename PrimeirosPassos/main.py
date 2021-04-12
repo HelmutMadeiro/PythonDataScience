57818518febcee785190518585e849fc401af9aa
Helmut Madeiro <HelmutMadeiro@users.noreply.github.com>
2021-04-12 00:35:33 -0300

    Update main.py

--- PrimeirosPassos/main.py
+++ PrimeirosPassos/main.py
@@ -1,5 +1,17 @@
 def saudacao(nome):
-    return print(f'seu nome é {nome}')
+    return print(f'Olá  {nome} tudo bom?')
+
+
+def verifica_se_pode_dirigir(idade):
+    idade = int(idade)
+    if idade >= 18:
+        print("você tem idade para dirigir")
+    else:
+        print("Você não tem idade para dirigir")
+
 
 nome = input('Qual seu nome? ')
 saudacao(nome)
+
+idade = input('qual a sua idade?')
+verifica_se_pode_dirigir(idade)
