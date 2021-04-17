4364a2b756ef49dc057bf70d28ca993771f46ac7
Helmut Madeiro <HelmutMadeiro@users.noreply.github.com>
2021-04-17 18:14:05 -0300

    Update For.py

--- IntroducaoLinguagemNumpy/For.py
+++ IntroducaoLinguagemNumpy/For.py
@@ -2,5 +2,11 @@
 import numpy as np
 peso = np.array([106.0, 68.5, 75.0])
 altura = np.array([1.9, 1.53, 1.75])
-IMC = peso / altura ** 2
-print(IMC)
+
+
+for p in peso:
+    for a in altura:
+        print( p,a)
+
+x = p / altura ** 2
+print(a,p,x, 'aaa')
