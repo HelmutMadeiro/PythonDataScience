0e936089f929eaa960d881cabfa5d463f96f371a
Helmut Madeiro <HelmutMadeiro@users.noreply.github.com>
2021-04-16 23:32:52 -0300

    .

---
+++ IntroducaoLinguagemNumpy/ArrayNumpy.py
@@ -0,0 +1,63 @@
+from time import time
+
+import numpy as np
+# buscar apenas uma funcao
+# podemos importar somente as funções que iremos utilizar. Para isso,
+# basta usarmos a instrução from seguida do nome da biblioteca,
+# a palavra-chave import e a função desejada - nesse caso, arange().
+from numpy import arange
+np.arange(10)
+km = np.array ([1000,2300,4987,1500])
+print(km)
+
+dados = [
+    ['Rodas de liga', 'Travas elétricas', 'Piloto automático', 'Bancos de couro', 'Ar condicionado', 'Sensor de estacionamento', 'Sensor crepuscular', 'Sensor de chuva'],
+    ['Central multimídia', 'Teto panorâmico', 'Freios ABS', '4 X 4', 'Painel digital', 'Piloto automático', 'Bancos de couro', 'Câmera de estacionamento'],
+    ['Piloto automático', 'Controle de estabilidade', 'Sensor crepuscular', 'Freios ABS', 'Câmbio automático', 'Bancos de couro', 'Central multimídia', 'Vidros elétricos']
+]
+dados
+
+
+# importação de um txt
+km = np.loadtxt(fname = 'data\carros-km.txt',dtype = int)
+print (km)
+
+Acessorios = np.array(dados)
+
+print(dados)
+#saber a dimensao do array (linas,colunas )
+print(Acessorios.shape)
+print(km.shape)
+
+
+
+#é possivel fazer calculos nos arrays em np
+
+km = np.array([44410., 5712., 37123., 0., 25757.])
+anos = np.array([2003, 1991, 1990, 2019, 2006])
+
+idade = []
+idade.append(2019 - anos)
+print (idade)
+
+km_media = km / idade
+print(km_media)
+
+
+dados = np.array([km,anos])
+
+print(dados,'teste' )
+
+#0 pego o primeira linha  arrei e 1 segunda linha
+km_media = dados[0] / (2019 - dados[1])
+print(km_media)
+
+
+
+
+
+
+
+
+
+
--- IntroducaoLinguagemNumpy/CondicionaisLacos.py
+++ IntroducaoLinguagemNumpy/CondicionaisLacos.py
@@ -19,3 +19,100 @@ print (quadrado)
 
 print([1 ** 2 for i in range(10)])
 
+Acessorio = []
+for lista in dados:
+    for item in lista:
+        Acessorio.append(item)
+print(Acessorio)
+#list comprehensions
+print(list(set([item for lista in dados for item in lista])))
+
+#remode duplicado
+list(set(Acessorio))
+
+# 1º item da lista - Nome do veículo
+# 2º item da lista - Ano de fabricação
+# 3º item da lista - Veículo é zero km?
+
+
+# usando o if e else
+dados = [
+    ['Jetta Variant', 2003, False],
+    ['Passat', 1991, False],
+    ['Crossfox', 1990, False],
+    ['DS5', 2019, True],
+    ['Aston Martin DB4', 2006, False],
+    ['Palio Weekend', 2012, False],
+    ['A5', 2019, True],
+    ['Série 3 Cabrio', 2009, False],
+    ['Dodge Jorney', 2019, False],
+    ['Carens', 2011, False]
+]
+
+
+zero_km_y = []
+for lista in dados:
+    if(lista[2] == True):
+        zero_km_y.append(lista)
+print(zero_km_y)
+
+zero_km_N = []
+for lista in dados:
+    if(lista[2] == False):
+        zero_km_N.append(lista)
+print(zero_km_N)
+
+print([zero_km_y for lista in dados if lista[2] == True])
+
+
+zero_km_y,zero_km_N = [],[]
+
+for lista in dados:
+    if(lista[2] == True):
+        zero_km_y.append(lista)
+    else:
+        zero_km_N.append(lista)
+
+print(zero_km_y,zero_km_N)
+
+
+A, B, C = [],[],[]
+for lista in dados:
+    if lista[1]<=2000:
+        A.append(lista)
+    elif lista[1]> 2000 and lista[1] <=2010:
+        B.append(lista)
+    else:
+        C.append(lista)
+
+print(A)
+print(B)
+print(C)
+
+A, B, C = [],[],[]
+for lista in dados:
+    if lista[1]<=2000:
+        A.append(lista)
+    elif (2000 > lista[1] <=2010):
+        B.append(lista)
+    else:
+        C.append(lista)
+
+
+
+pesos = [106.0, 68.5, 75.0]
+alturas =[1.9, 1.53, 1.75]
+
+imc = []
+for peso in pesos:
+    for altura in alturas:
+        imcs = peso / altura ** 2
+    imc.append(imcs)
+
+print (imc)
+
+import numpy as np
+peso = np.array([106.0, 68.5, 75.0])
+altura = np.array([1.9, 1.53, 1.75])
+IMC = peso / altura ** 2
+print(IMC)
