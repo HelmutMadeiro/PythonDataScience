3506dace58198bd147bbbbe88107ce49e464d421
Helmut Madeiro <HelmutMadeiro@users.noreply.github.com>
2021-04-24 22:17:36 -0300

    Create PandasBasico.py

---
+++ FuncoesPacotesPandasBasico/PandasBasico.py
@@ -0,0 +1,22 @@
+import pandas as pd
+carros = ['jetta variant', 'Passat', 'Crossfox']
+
+#as series, arrays unidimensionais rotulados capazes de armazenar qualquer tipo de dado.
+#Os rótulos das linhas de uma series são conhecidos como index,
+
+print(pd.Series(carros))
+
+dados = [
+    {'Nome': 'Jetta Variant', 'Motor': 'Motor 4.0 Turbo', 'Ano': 2003, 'Quilometragem': 44410.0, 'Zero_km': False, 'Valor': 88078.64},
+    {'Nome': 'Passat', 'Motor': 'Motor Diesel', 'Ano': 1991, 'Quilometragem': 5712.0, 'Zero_km': False, 'Valor': 106161.94},
+    {'Nome': 'Crossfox', 'Motor': 'Motor Diesel V8', 'Ano': 1990, 'Quilometragem': 37123.0, 'Zero_km': False, 'Valor': 72832.16}
+]
+
+dataset = pd.DataFrame(dados)
+
+print(dataset)
+
+
+
+
+
