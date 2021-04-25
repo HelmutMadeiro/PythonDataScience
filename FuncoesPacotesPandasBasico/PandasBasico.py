50c796366b690261f533f452321dd38a8587b889
Helmut Madeiro <HelmutMadeiro@users.noreply.github.com>
2021-04-25 00:12:25 -0300

    .

--- FuncoesPacotesPandasBasico/PandasBasico.py
+++ FuncoesPacotesPandasBasico/PandasBasico.py
@@ -4,7 +4,7 @@ carros = ['jetta variant', 'Passat', 'Crossfox']
 #as series, arrays unidimensionais rotulados capazes de armazenar qualquer tipo de dado.
 #Os rótulos das linhas de uma series são conhecidos como index,
 
-print(pd.Series(carros))
+pd.Series(carros)
 
 dados = [
     {'Nome': 'Jetta Variant', 'Motor': 'Motor 4.0 Turbo', 'Ano': 2003, 'Quilometragem': 44410.0, 'Zero_km': False, 'Valor': 88078.64},
@@ -12,11 +12,22 @@ dados = [
     {'Nome': 'Crossfox', 'Motor': 'Motor Diesel V8', 'Ano': 1990, 'Quilometragem': 37123.0, 'Zero_km': False, 'Valor': 72832.16}
 ]
 
-dataset = pd.DataFrame(dados)
+dataset3 = pd.DataFrame(dados)
 
-print(dataset)
+pd.set_option('display.max_rows',10)
+pd.set_option('display.max_columns',100)
+
+dataset = pd.read_csv ('dados/db.csv',sep =';',index_col=0)
+
+dataset = pd.DataFrame(dataset)
+#dataset.dtypes
+#dataset.info()
+#dataset[['Quilometragem','Valor']].describe()
 
 
+
+dataset.head(2)
+print(dataset[['Valor']])
 
 
 
